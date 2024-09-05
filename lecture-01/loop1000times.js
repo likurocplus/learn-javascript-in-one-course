d0ba96904fe5c0a972310f377e209d07e9f0bc38
Khoi Quoc <likuroquoc@gmail.com>
2024-09-05 14:44:57 +0700

    add lecture-01 ~~

---
+++ lecture-01/loop1000times.js
@@ -0,0 +1,13 @@
+// Function definition: loop1000times
+// Input: A string 'str' to be printed 1000 times
+function loop1000times(str) {
+    // Process: Iterate from 0 to 999 (total 1000 iterations)
+    for (let index = 0; index < 1000; index++) {
+        // Output: Print the string in each iteration
+        console.log(str);
+    }
+}
+
+// Call the function with the string "I love you"
+loop1000times("I love you"); //Output: "I love you" 1000 times
+
