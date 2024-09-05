9f6b9cfee084d25f2f819aac120b9b4bc2ab1dc6
Khoi Quoc <likuroquoc@gmail.com>
2024-09-06 01:11:42 +0700

    add new lecture 2 ~~

--- lecture-01/loop1000times.js
+++ lecture-01/loop1000times.js
@@ -11,3 +11,40 @@ function loop1000times(str) {
 // Call the function with the string "I love you"
 loop1000times("I love you"); //Output: "I love you" 1000 times
 
+// ----- example of chaos programming with no language or standard
+
+// coder1
+// function loop 1000 times (str) {
+//     loops (1000) {
+//         write(str);
+//     }
+// }
+// // coder2
+// function square {str} {
+//     for (let index = 0; index < 1000; index++) {
+//         console.log(str);
+//     }
+// }
+
+// no computer nor human can understand either coder1 or coder2 way
+
+// ----- Javascript (standard) way to define a function
+
+function loop1000times(str) {
+    for (let index = 0; index < 1000; index++) {
+        console.log(str);
+    }
+}
+// now computer and everyone can understand
+
+// my thought: I want to print any string 1000 times
+// express my thought in Javascript language
+function loop1000times(str) {
+    for (let index = 0; index < 1000; index++) {
+      console.log(str);
+    }
+  }
+  
+  // now the computer can print the string for me whenever I tell it to
+  loop1000times("I love you");
+  
---
+++ lecture-02/ipo.js
@@ -0,0 +1,6 @@
+console.log("III  PPPP  OOOO");
+console.log(" I   P  P  O  O");
+console.log(" I   PPPP  O  O");
+console.log(" I   P     O  O");
+console.log(" I   P     O  O");
+console.log("III  P     OOOO");
