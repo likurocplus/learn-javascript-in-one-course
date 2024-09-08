0512de9c3c1faa281c3207c7daf8036686034c15
Khoi Quoc <likuroquoc@gmail.com>
2024-09-08 14:30:00 +0700

    add code example in lecture-03

---
+++ lecture-03/syntax.js
@@ -0,0 +1,43 @@
+//1.White space
+let a = 10;
+let b =                                 10; //not wrong but very ugly =)))
+console.log(b);
+
+
+//2. Comment
+let c = 10;//Comment inline
+//Comment single line
+let d = 10;
+
+/* 
+Block
+line
+*/
+
+//3.semi colon
+let e = 10; 
+let f = 10    //don't wrong
+
+//4. Identifier
+
+let myVarr;
+let $myVar;
+let _myVar;
+
+//5.Case sentitive identifier 
+
+let myVar; 
+let MYVAR;
+let myvar;
+
+//variable declaration
+const pi = 3.14;
+let number;
+var number2;
+
+//hoisting
+var x;
+console.log(x); // undefined
+x = 5;
+console.log(x); // 5
+
