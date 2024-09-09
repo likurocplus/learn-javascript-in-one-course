b8f4733ca38516e1e56e3ce4aa27c67ac6c96a36
Khoi Quoc <likuroquoc@gmail.com>
2024-09-09 21:25:06 +0700

    add file into lecture-06

---
+++ lecture-06/regex.js
@@ -0,0 +1,5 @@
+// regex1 and regex2 are Regular Expression in Javascript
+let regex1 = /luong*.+/g
+let regex2 = new RegExp('luong*.+', 'g') // we will talk more about this syntax later
+console.log('regex1:', regex1) // regex1: /luong*.+/g
+console.log('regex2:', regex2) // regex2: /luong*.+/g
