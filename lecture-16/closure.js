68d623c38624847622858954e2814a0c28a94c55
Khoi Quoc <likuroquoc@gmail.com>
2024-09-17 01:38:17 +0700

    add lecture 16

---
+++ lecture-16/closure.js
@@ -0,0 +1,41 @@
+//EX1
+function numberGenerator() {
+    // Local free variable that ends up within the closure
+    var num = 1;
+    function checkNumber() { 
+      console.log(num);
+    }
+    num++;
+    return checkNumber;
+  }
+  
+  var number = numberGenerator();
+  number(); // 2
+
+
+  //EX2
+  function outer() {
+    let count = 0;
+    return function() {
+      count++;
+      return count;
+    };
+  }
+  
+  const counter = outer();
+  console.log(counter());  // Output: 1
+  console.log(counter());  // Output: 2
+
+  function counter() {
+    let count = 0; // shared data
+  
+    return function() {
+      count++; // modify shared data
+      return count;
+    };
+  }
+  
+  const increment = counter();
+  console.log(increment()); // 1
+  console.log(increment()); // 2
+  
