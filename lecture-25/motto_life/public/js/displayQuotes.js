e7f36c7f8e5ec9f329d31af72a31ad792147cf49
Khoi Quoc <likuroquoc@gmail.com>
2024-09-22 22:19:54 +0700

    add lecture 25 - project

---
+++ lecture-25/motto_life/public/js/displayQuotes.js
@@ -0,0 +1,18 @@
+//Input: data fetched from fetchAPI- type: Object
+//Process:
+//- call displayQuote when have data fetched
+//- get element by Id from index.html
+//- inner new data text to elements
+//Output: display data fetched to screen (index.html)
+const displayQuote = (data) => {
+    //step1: get element by id to handle
+    const quoteElement = document.getElementById("quote");
+    const authorElement = document.getElementById("author");
+
+    //inner data text to element to display the screen
+    quoteElement.innerText = data.quote; 
+    authorElement.innerText = `— ${data.author}`;
+};
+
+//export display function
+export default displayQuote;
---
+++ lecture-25/motto_life/public/js/script.js
@@ -0,0 +1,27 @@
+import displayQuote from "./displayQuotes.js"; // Import the displayQuote function
+
+const API_URL = "https://dummyjson.com/quotes/random"; // Define the URL for the API
+
+// Input: None (User action through click event triggers fetch)
+// Process: 
+// - call fetchQuote function when button is click
+// - fetch data from API (URL: https://dummyjson.com/quotes/random)
+// - convert the response to JSON
+// - pass the data to displayQuote function for render
+// Output: render quote and author displayed on the webpage
+const fetchQuote = async () => { // 3. Define fetchQuote as an async function
+    try {
+        // step 1: Call the API and waiting for a response
+        const resp = await fetch(API_URL); // 4. Fetch data from the defined API URL
+        // step 2: Convert the response to JSON format
+        const data = await resp.json(); // 5. Convert the API response to a JSON object
+        // step 3: Call the displayQuote function to render the data on the page
+        displayQuote(data); // pass the data fetched from the API to the displayQuote function
+    } catch (error) {
+        // step 4: Catch and handle any errors
+        console.error('Error fetching data:', error); // 7. Log an error message if fetching fails
+    }
+};
+
+// step 5: Add an event to button, trigger fetchQuote when click
+document.getElementById("fetch-quote").addEventListener("click", fetchQuote);
